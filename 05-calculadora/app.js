0b3c9ccfc355c6a69e1224622e98bc6f1f17a760
Jéssica Avelar <89416633+jessicaavelar@users.noreply.github.com>
2022-06-03 14:04:09 -0300

    substituir arquivos

--- 05-calculadora/app.js
+++ 05-calculadora/app.js
@@ -1,37 +1,90 @@
 // Selecionar a entrada do usuáio
 let entrada = document.querySelector( "input" );
 
-// Selecionar a caixa d
+// Selecionar a caixa
 let seletor = document.querySelector( "select" );
 
+// Selecionar a didv resultado
 let resultado = document.querySelector( ".resultado" );
 
-// Ouvir o evento de entrada - quando ousuário seleciona
+let quantidadeCarne, quandidadeDias;
+
+let formata = new Intl.NumberFormat('pt-BR');
+
+
+// Ouvir o evento de entrada - quando ousuário digita e seleciona
 entrada.addEventListener( "input", validar );
 
 seletor.addEventListener( "change", funcao );
 
-function validar(e) {
 
-    console.log(e.target.value)
+function validar () {
+    
+    // Transformar o valor do usuário em inteiro
+    let valor = parseInt( entrada.value );
+
+    // Validar o valor do usuário
+    if ( valor >= 0 ) {
+
+        // Inserir o valor digitado pelo usuário
+
+        quantidadeCarne = valor;
+        calcularQuantidade();
+
+    }
 
 }
 
 
-function funcao(e) {
+function funcao (e) {
 
-    console.log(e.target.value)
+    let dias = parseInt( e.target.value );
 
-    resultado.style.display = "initial"
+    quandidadeDias = dias;
+
+    calcularQuantidade();
 
 }
+
+
+function calcularQuantidade() {
+
+    if ( quandidadeDias && quantidadeCarne ) {
+
+        let semana = quandidadeDias * quantidadeCarne;
+        let ano = semana * 52;
+        let kg = ano * 0.001;
+        let agua = kg * 15000;
+        let metano = kg * 2;
+        let co2 = kg * 50;
+
+        atualizaTexto(kg, agua, metano, co2);
+
+    }
+
+}
+
+
+function atualizaTexto(kg, agua, metano, co2) {
+
+    resultado.style.display = "initial";
+
+    document.querySelector("[data-texto='quantidade']").innerText = formata.format(quantidadeCarne);
+
+    document.querySelector("[data-texto='dias']").innerText = formata.format(quandidadeDias);
+
+    document.querySelector("[data-texto='anual']").innerText = formata.format(kg);
+
+    document.querySelector("[data-texto='agua']").innerText = formata.format(agua);
+
+    document.querySelector("[data-texto='metano']").innerText = formata.format(metano);
+
+    document.querySelector("[data-texto='co2']").innerText = formata.format(co2);
+
+}
+
 
 /*
-
-Mostrar a resposta
-
-    resultado.style.display = "initial"
-
 
 Inserir a resposta do usuário na primeira linha
 
