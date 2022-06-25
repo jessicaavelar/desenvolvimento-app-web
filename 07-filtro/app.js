eeaf9572e6b14ece1534193dc094e3d6a41993cf
Jéssica Avelar <89416633+jessicaavelar@users.noreply.github.com>
2022-06-25 16:45:06 -0300

    substituir arquivos

--- 07-filtro/app.js
+++ 07-filtro/app.js
@@ -1,92 +1,233 @@
-let dados = [
-    {
-      "tipo-crime": "Ameaça de morte",
-      "ano": 2021,
-      "estado": "Amazonas",
-      "municipio": "Manaus",
-      "localização": "Aldeia Tsetsu Davi/Km 23/BR-174",
-      "data": "05/12/2021",
-      "nome": "Cacique da Aldeia Tsetsu Davi",
-      "vitimas": 1
-    },
+let arquivo
 
-    {
-        "tipo-crime": "Assassinato",
-        "ano": 2020,
-        "estado": "Bahia",
-        "municipio": "Paulo Afonso",
-        "localização": "Aldeia Tsetsu Davi/Km 23/BR-174",
-        "data": "05/12/2021",
-        "nome": "Cacique da Aldeia Tsetsu Davi",
-        "vitimas": 3
-    },
-
-    {
-        "tipo-crime": "Tentativa de assassinato",
-        "ano": 2019,
-        "estado": "Amazonas",
-        "municipio": "Manaus",
-        "localização": "Aldeia Tsetsu Davi/Km 23/BR-174",
-        "data": "05/12/2021",
-        "nome": "Cacique da Aldeia Tsetsu Davi",
-        "vitimas": 10
-    }
-
-]
-
+// Seleciona a tabela
 let tabela = document.querySelector( "table" )
 
-let seletorCategoria = document.querySelector( "select[name='categoria']" );
+// Seleciona o tipo de crime
+let seletorCategoria = document.querySelector( "select[name='tipo-crime']" );
 
+// Seleciona o ano
 let seletorAno = document.querySelector( "select[name='ano']" );
 
+// Seleciona o estado
 let seletorEstado = document.querySelector( "select[name='estado']" );
 
-seletorCategoria.addEventListener( "change", teste );
+// Ouve os eventos de mudança
+seletorCategoria.addEventListener( "change", mostrar );
 
-seletorAno.addEventListener( "change", teste );
+seletorAno.addEventListener( "change", mostrar );
 
-seletorEstado.addEventListener( "change", teste );
+seletorEstado.addEventListener( "change", mostrar );
 
 
+// Lê o arquivo json
+fetch ( "arquivo.json" )
+    .then( resposta => resposta.json() )
+    .then( dados => {
 
-function teste() {
+        arquivo = dados
+        mostrar()
+        preencheSeletores()
 
-    limpar()
+    } )
 
-    let filtrados = dados.filter( checar );
-    for ( let filtrado of filtrados ) {
 
-        let linha = document.createElement( "tr" );
-        let celulaCategoria = document.createElement ("td");
-        tabela.append( linha );
-        linha.append( celulaCategoria )
-        celulaCategoria.textContent = filtrado["tipo-crime"]
+// Função para preencher os seletores do usuário com os valores da planilha
+function preencheSeletores() {
 
+    // Cria as listas de categoria
+    let listaCategoria = arquivo.map( c => c[ "tipo-crime" ]);
+
+    let listaAno = arquivo.map( a => a[ "ano" ]);
+
+    let listaEstado = arquivo.map( e => e[ "estado" ]);
+
+
+    // Seleciona os valores únicos nas listas
+    listaCategoria = valoresUnicos( listaCategoria );
+
+    listaAno = valoresUnicos( listaAno );
+
+    listaEstado = valoresUnicos( listaEstado );
+
+
+    // Adiciona os tipos de crime no seletor
+    for ( categoria of listaCategoria ) {
+
+        // Cria o elemento "option"
+        let novaOpcao = document.createElement( "option" );
+
+        // Insere o valor "Tipo de Crime"
+        novaOpcao.textContent = categoria;
+
+        // Adiciona o tipo de crime também ao "value"
+        novaOpcao.value = categoria;
+
+        // Adiciona o filho ao elemento
+        seletorCategoria.appendChild( novaOpcao );
+
+    }
+
+    // Adiciona os anos no seletor
+    for ( ano of listaAno ) {
+
+        // Cria o elemento "option"
+        let novaOpcao = document.createElement( "option" );
+
+        // Insere o valor "Ano"
+        novaOpcao.textContent = ano;
+
+        // Adiciona o ano também ao "value"
+        novaOpcao.value = ano;
+
+        // Adiciona o filho ao elemento
+        seletorAno.appendChild( novaOpcao );
+    }
+
+    // Adiciona os estados no seletor
+    for ( estado of listaEstado ) {
+
+        // Cria o elemento "option"
+        let novaOpcao = document.createElement( "option" );
+
+        // Insere o valor "Estado"
+        novaOpcao.textContent = estado;
+
+        // Adiciona o estado também ao "value"
+        novaOpcao.value = estado;
+
+        // Adiciona o filho ao elemento
+        seletorEstado.appendChild( novaOpcao );
     }
 
 }
 
 
-function checar( dado ) {
+// Função que seleciona os valores únicos da lista
+function valoresUnicos( lista ){
 
+    return lista.filter( ( value, index, array ) => array.indexOf( value ) == index )
+
+}
+
+
+// Função para mostrar os dados
+function mostrar(){
+
+    // Primeiro chama a função para limpar 
+    limpar()
+
+    // Utiliza o método filter do JS e armazena na variável os que passaram pela checagem
+    let filtrados = arquivo.filter( checar );
+
+
+    // Utilizar o filtrados para fazer a soma
+
+        for ( let filtrado of filtrados ) {
+
+        // Cria os elementos da tabela
+        let linha = document.createElement( "tr" );
+        let celulaTcrime = document.createElement( "td" );
+        let celulaAno = document.createElement( "td" );
+        let celulaEstado = document.createElement( "td" );
+        let celulaMunicipio = document.createElement( "td" );
+        let celulaLocal = document.createElement( "td" );
+        let celulaData = document.createElement( "td" );
+        let celulaNome = document.createElement( "td" );
+        let celulaVitimas = document.createElement( "td" );
+        let celulaCategoria = document.createElement( "td" );
+
+        // Adiciona os elemento à tabela
+        tabela.append( linha );
+        linha.append( celulaTcrime );
+        linha.append( celulaAno);
+        linha.append( celulaEstado );
+        linha.append( celulaMunicipio );
+        linha.append( celulaLocal);
+        linha.append( celulaData );
+        linha.append( celulaNome );
+        linha.append( celulaVitimas );
+        linha.append( celulaCategoria );
+        
+        // Insere os valores
+        celulaTcrime.textContent = filtrado[ "tipo-crime" ];
+        celulaAno.textContent = filtrado[ "ano" ];
+        celulaEstado.textContent = filtrado[ "estado" ];
+        celulaMunicipio.textContent = filtrado[ "municipio" ];
+        celulaLocal.textContent = filtrado[ "localização" ];
+        celulaData.textContent = filtrado[ "data" ];
+        celulaNome.textContent = filtrado[ "nome" ];
+        celulaVitimas.textContent = filtrado[ "vitimas" ];
+        celulaCategoria.textContent = filtrado[ "categoria" ];
+        
+    }
+}
+
+
+// Função para checar a filtragem dos valores
+function checar( dado ){
+
+    // Cria as variáveis com as seleções do usuário
     let categoria = seletorCategoria.value;
+
     let ano = seletorAno.value;
+
     let estado = seletorEstado.value;
 
-    if ( dado[ "tipo-crime" ] == categoria ) { // checar as outras condições aqui com o &&
+    // Cria a lista de filtros
+    let filtroCategoria , filtroAno, filtroEstado;
+
+
+    // Verifica se atente às condições
+    if ( categoria == "todos" ) {
+
+        filtroCategoria = true
+    }
+
+    else {
+
+        filtroCategoria = dado[ "tipo-crime" ] == categoria
+
+    }
+
+
+    if ( ano == "todos" ) {
+
+        filtroAno = true
+
+    }
+
+    else {
+        filtroAno = dado[ "ano" ] == ano
+    }
+
+
+    if ( estado == "todos" ) {
+
+        filtroEstado = true
+
+    }
+
+    else {
+
+        filtroEstado = dado[ "estado" ] == estado
+
+    }
+
+    // Checa o que foi selecionado pelo usuário
+    if ( filtroCategoria && filtroAno && filtroEstado ) {
 
         return true;
 
     }
 
-
 }
 
+
+// Função para limpar os dados da tabela e criar o cabeçalho
 function limpar(){
 
-    tabela.textContent = ""
-    // Criar aqui o cabeçalho utilizando o innerHTML
+    // Limpa os dados e insere o cabeçalho novamente
+    tabela.innerHTML = "<tr><th>Tipo de crime</th><th>Ano</th><th>Estado</th><th>Município</th><th>Localização</th><th>Data</th><th>Nome</th><th>Vítimas</th><th>Categoria</th></tr>"
 
 }
-
